477ba5973ef7c8e81c73db5cd25a6d7a35272af9
kostaszazim <kostas_zazim@yahoo.gr>
2022-02-21 21:51:39 +0200

    Added non existing taxonomies to prototype product at the end

--- wp-content/plugins/tmdb_integration/woocommerce/classes/tmdb-import-simple-product.php
+++ wp-content/plugins/tmdb_integration/woocommerce/classes/tmdb-import-simple-product.php
@@ -6,6 +6,7 @@ if (!defined('ABSPATH')) {
 
 class TMDB_Import_Simple_Product extends TMDB_Import_Prototype
 {
+    private $last_position;
     protected function update_product_attributes()
     {
         $new_attributes = [];
@@ -15,19 +16,46 @@ class TMDB_Import_Simple_Product extends TMDB_Import_Prototype
                     $new_wc_attribute = new WC_Product_Attribute();
                     $new_wc_attribute->set_id($product_attribute->get_id());
                     $new_wc_attribute->set_name($product_attribute->get_name());
-                    $new_wc_attribute->set_options(array_map(function ($element) {
+                    $new_wc_attribute->set_options(
+                        array_map(function ($element) {
                             return (int) $element;
-                         }, $this->tmdb_movie_info[$pa_name]));
+                        }, $this->tmdb_movie_info[$pa_name]),
+                    );
                     $new_wc_attribute->set_position($product_attribute->get_position());
                     $new_wc_attribute->set_visible($product_attribute->get_visible());
-                    $new_wc_attribute->set_variation($product_attribute->get_variation());     
+                    $new_wc_attribute->set_variation($product_attribute->get_variation());
                 }
-                // $product_attribute->set_options(array_map(function ($element) {
-                //     return (int) $element;
-                // }, $this->tmdb_movie_info[$pa_name]));
             }
 
-           $new_attributes[$pa_name] = $new_wc_attribute;
+            $new_attributes[$pa_name] = $new_wc_attribute;
+            $this->last_position = $new_wc_attribute->get_position();
+        }
+
+        $existing_attribute_keys = array_keys($this->product_attributes);
+        $missing_attributes = array_filter(
+            $this->tmdb_movie_info,
+            function ($element, $key) use ($existing_attribute_keys) {
+                return !in_array($key, $existing_attribute_keys) && strpos($key, 'pa_') !== false;
+            },
+            ARRAY_FILTER_USE_BOTH,
+        );
+
+        if (!empty($missing_attributes)) {
+            foreach ($missing_attributes as $tax_name => $attributes) {
+                $this->last_position++;
+                $attribute_obj = new WC_Product_Attribute();
+                $attribute_obj->set_id(wc_attribute_taxonomy_id_by_name($tax_name));
+                $attribute_obj->set_name($tax_name);
+                $attribute_obj->set_options(
+                    array_map(function ($element) {
+                        return (int) $element;
+                    }, $attributes),
+                );
+                $attribute_obj->set_position($this->last_position);
+                $attribute_obj->set_variation(false);
+                $attribute_obj->set_visible(true);
+                $new_attributes[$tax_name] = $attribute_obj;
+            }
         }
 
         $this->created_product->set_attributes($new_attributes);
